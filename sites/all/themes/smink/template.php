c165d23464e76d34793c27603693653adce5c196
István Palócz <istvan@palocz.hu>
2015-04-22 08:19:13 +0200

    Initial

---
+++ sites/all/themes/smink/template.php
@@ -0,0 +1,18 @@
+<?php
+
+function smink_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+
+    $output .= '<div class="breadcrumb">' . implode(' * ', $breadcrumb) . '</div>';
+    return $output;
+  }
+}
+
+function smink_preprocess_block(&$variables) {
+  $variables['classes_array'][] = 'smink';
+}
